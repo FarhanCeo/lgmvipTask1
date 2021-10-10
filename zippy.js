7b59a6e14dffe9f22d1684034f1efffdd951119d
Farhan Ahmed <80202023+FarhanCeo@users.noreply.github.com>
2021-10-10 18:52:18 +0530

    Add files via upload

---
+++ zippy.js
@@ -0,0 +1,60 @@
+function mover1(){
+   document.getElementById('home').style.width="80px";
+  
+    document.getElementById('home').style.borderRadius="25%";
+    document.getElementById('home').style.backgroundColor="white";
+    document.getElementById('home').style.textAlign="center";   
+}
+function mover2(){
+    document.getElementById('projects').style.width="80px";
+   
+     document.getElementById('projects').style.borderRadius="25%";
+     document.getElementById('projects').style.backgroundColor="white";
+     document.getElementById('projects').style.textAlign="center";   
+ }
+ function mover3(){
+    document.getElementById('pricing').style.width="80px";
+     
+     document.getElementById('pricing').style.borderRadius="25%";
+     document.getElementById('pricing').style.backgroundColor="white";
+     document.getElementById('pricing').style.textAlign="center";   
+ }
+ function mover4(){
+    document.getElementById('team').style.width="80px";
+    
+     document.getElementById('team').style.borderRadius="25%";
+     document.getElementById('team').style.backgroundColor="white";
+     document.getElementById('team').style.textAlign="center";   
+ }
+ function mover5(){
+    document.getElementById('blog').style.width="80px";
+    
+     document.getElementById('blog').style.borderRadius="25%";
+     document.getElementById('blog').style.backgroundColor="white";
+     document.getElementById('blog').style.textAlign="center";   
+ }
+ function mover6(){
+    document.getElementById('contact').style.width="80px";
+     
+     document.getElementById('contact').style.borderRadius="25%";
+     document.getElementById('contact').style.backgroundColor="white";
+     document.getElementById('contact').style.textAlign="center";   
+ }
+ function mdown1(){
+    document.getElementById('home').style.backgroundColor="";
+ }
+ function mdown2(){
+    document.getElementById('projects').style.backgroundColor="";
+ }
+ function mdown3(){
+    document.getElementById('pricing').style.backgroundColor="";
+ }
+ function mdown4(){
+    document.getElementById('team').style.backgroundColor="";
+ }
+ function mdown5(){
+    document.getElementById('blog').style.backgroundColor="";
+ }
+ function mdown6(){
+    document.getElementById('contact').style.backgroundColor="";
+ }
